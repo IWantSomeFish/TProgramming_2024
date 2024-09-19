21d836a4af54e0ee0e25d9d3e85e7cfe6d9ff8ff
Vinogradov Anton <vino74974@gmail.com>
2024-09-19 14:30:35 +0300

    feat: taskB tests

--- rpgsaga/saga/tests/example.spec.ts
+++ rpgsaga/saga/tests/example.spec.ts
@@ -1,7 +1,19 @@
-describe('Example', () => {
-    it('should return 5 as result of 2 and 3 sum', () => {
-        const a: number = 3;
-        const b: number = 2;
-        expect(a + b).toEqual(5);
+import { calculateFunc,taskA,taskB } from "../src";
+
+describe('TaskB func tests', () => {
+    it('should return 5 values', () => {
+        expect(taskB([1,2,3,4,5])).toEqual(5);
+    })
+    it('Empty array. Should return 0 values', () => {
+        expect(taskB([])).toEqual(0);
     })
 });
+
+describe('TaskA func', ()=>{
+    it('should return 5 value',()=>{
+        expect(taskA(1,6,1)).toEqual(5)
+    })
+    it('The beginning is less than the end, the step is negative',()=>{
+        expect(taskA(5,0,-1)).toEqual(5)
+    })
+})
