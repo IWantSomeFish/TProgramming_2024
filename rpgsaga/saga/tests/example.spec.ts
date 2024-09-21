0444240a8e557f5933ac8cd570b0fcf74bb4f2c7
Vinogradov Anton <vino74974@gmail.com>
2024-09-21 10:31:27 +0300

    feat: calculaion func tests

--- rpgsaga/saga/tests/example.spec.ts
+++ rpgsaga/saga/tests/example.spec.ts
@@ -1,10 +1,10 @@
 import { calculateFunc,taskA,taskB } from "../src";
 
 describe('TaskB func tests', () => {
-    it('Array with len = 5, should return 5 values', () => {
+    it('Array with len = 5', () => {
         expect(taskB([1,2,3,4,5])).toEqual(5);
     })
-    it('Empty array. Should return 0 values', () => {
+    it('Empty array', () => {
         expect(taskB([])).toEqual(0);
     })
 });
@@ -13,25 +13,31 @@ describe('TaskA func', ()=>{
     it('The basic case',()=>{
         expect(taskA(1,6,1)).toEqual(5)
     })
-    // it('The basic case, but the step is negative (its realy inifinity)',()=>{
-    //     expect(taskA(1,6,-1)).toEqual(Infinity)
-    // })
-    it('The beginning is less than the end, the step is negative,should return 0',()=>{
+    it('The basic case, but the step is negative',()=>{
+        expect(taskA(1,6,-1)).toEqual(0)
+    })
+    it('The beginning is less than the end, the step is negative',()=>{
         expect(taskA(5,1,-1)).toEqual(0)
     })
-    it('The beginning is less than the end, the step is positive,should return 0',()=>{
+    it('The beginning is less than the end, the step is positive',()=>{
         expect(taskA(5,1,1)).toEqual(0)
     })
-    it('The beginnig is equal to the end, should return 0',()=>{
+    it('The beginnig is equal to the end',()=>{
         expect(taskA(1,1,1)).toEqual(0)
     })   
 })
 
 describe('Calculation func tests', () => {
-    it('Should return 3', () => {
-        expect(calculateFunc(3,18,3)).toEqual(2.48);
+    it('Basic case', () => {
+        expect(calculateFunc(3,18,3)).toEqual(13.18);
     })
-    it('ln from zero, should return 0, cause the divider = -Infinity', ()=>{
+    it('lg from zero', ()=>{
         expect(calculateFunc(1,7,0)).toEqual(0)
+    })
+    it('Negative A and B', () => {
+        expect(calculateFunc(-3,-18,3)).toEqual(NaN);
+    })
+    it('Negative X', () => {
+        expect(calculateFunc(3,18,-3)).toEqual(NaN);
     })
 });
