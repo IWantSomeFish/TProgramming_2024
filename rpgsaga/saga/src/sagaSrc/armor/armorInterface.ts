8efcde9c1a68829947f328643207ea4864474f8b
avinogrado <vino74974@gmail.com>
2024-10-30 10:50:42 +0300

    feat: armor abstract class

--- rpgsaga/saga/src/sagaSrc/armor/armorInterface.ts
+++ rpgsaga/saga/src/sagaSrc/armor/armorInterface.ts
@@ -1,11 +1,43 @@
 abstract class Armor {
-  abstract name: string;
-  abstract baseDefence: number;
-  abstract currentDefence: number;
+  name: string;
+  private _baseDefence: number;
+  private _currentDefence: number;
+  private _elementDefType: string;
 
-  reduceDamage(damage: number): number{
-    damage = damage - this.currentDefence
-    return damage
+  constructor(name: string, baseDefence: number, elementDefType: string) {
+    this.name = name;
+    this._baseDefence = baseDefence;
+    this._currentDefence = baseDefence;
+    this._elementDefType = elementDefType;
+  }
+
+  public set defence(amount: number) {
+    this._currentDefence = amount;
+  }
+
+  reduceDamage(damage: number, damageType: string): number {
+    let result: number;
+    if (damageType === this._elementDefType) {
+      result = 0;
+    } else {
+      result = damage - this._currentDefence;
+      if (result < 0) {
+        result = 0;
+      }
+    }
+    return result;
+  }
+
+  changeDefence(defence: number) {
+    if (this._currentDefence + defence <= 0) {
+      this._currentDefence = 0;
+    } else {
+      this._currentDefence = this._currentDefence + defence;
+    }
+  }
+
+  setDefenceDefault() {
+    this._currentDefence = this._baseDefence;
   }
 }
 export default Armor;
