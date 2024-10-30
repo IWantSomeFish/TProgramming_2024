b61f9e11ea92306cbd41f1e8c5fd530f6b1629ee
avinogrado <vino74974@gmail.com>
2024-10-30 21:09:31 +0300

    fix: changed mage weapon type to mageWeapon

--- rpgsaga/saga/src/sagaSrc/character/mage.ts
+++ rpgsaga/saga/src/sagaSrc/character/mage.ts
@@ -1,10 +1,11 @@
 import Armor from '../armor/armorInterface';
 import KnightWeapon from '../weapon/knightWeapon/knightWeapon';
+import MageWaepon from '../weapon/mageWeapon/mageWeapon';
 
 import Character from './characterAbstract';
 
 class Mage extends Character {
-  constructor(name: string, maxHP: number, armor: Armor, weapon: KnightWeapon) {
+  constructor(name: string, maxHP: number, armor: Armor, weapon: MageWaepon) {
     super(name, maxHP, armor, weapon);
   }
 }
