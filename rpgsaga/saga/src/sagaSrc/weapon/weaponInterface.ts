9d5c902ae9b1108c7bfeaf4ad9444044162f1b54
avinogrado <vino74974@gmail.com>
2024-10-31 14:44:00 +0300

    feat: added target for attack and specialAbility

--- rpgsaga/saga/src/sagaSrc/weapon/weaponInterface.ts
+++ rpgsaga/saga/src/sagaSrc/weapon/weaponInterface.ts
@@ -1,7 +1,10 @@
+import Character from "../character/characterAbstract";
+
 interface Weapon {
-  attack();
+  attack(target: Character);
   changeDamage(value: number): void;
   multiplyDamage(value: number): void;
   setDamageDefault(): void;
+  specialAbility(target: Character): any;
 }
 export default Weapon;
