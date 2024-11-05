5f3cde14379ebb7bf307378f3dcd77702f8df834
avinogrado <vino74974@gmail.com>
2024-11-05 20:21:37 +0300

    fix: index lint fix

--- rpgsaga/saga/src/index.ts
+++ rpgsaga/saga/src/index.ts
@@ -1,21 +1,20 @@
-import CharacterFabric from './sagaSrc/fabric/charactersFabrics/characterFabric';
-import Staff from './sagaSrc/weapon/mageWeapon/staffClass';
+import characterFabric from './sagaSrc/fabric/charactersFabrics/characterFabric';
+import staff from './sagaSrc/weapon/mageWeapon/staffClass';
 import animal from './polyLab/animal';
-import Cat from './polyLab/cat';
-import Dog from './polyLab/dog';
+import cat from './polyLab/cat';
+import dog from './polyLab/dog';
 
-const randCat = new Cat('Barsik', 5, 'White');
-const randDog = new Dog('Sharik', 10, 'Black');
+const randCat = new cat('Barsik', 5, 'White');
+const randDog = new dog('Sharik', 10, 'Black');
 const animalArray: animal[] = [randCat, randDog];
 for (let i = 0; i < animalArray.length; i++) {
   console.log(animalArray[i].makeSound());
   console.log(animalArray[i].toString());
 }
-const fabric = new CharacterFabric();
+const fabric = new characterFabric();
 const char = fabric.createChar('mage');
-const randstaff = new Staff('Staff', 10, 'fire');
+const randstaff = new staff('Staff', 10, 'fire');
 console.log(char);
 console.log(randstaff);
 randstaff.specialAbility(char);
 console.log(char);
-
