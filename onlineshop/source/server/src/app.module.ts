78f26e287f5b169d6517ed22d34ba78dd93fcef4
jskonst <jskonst@yandex.ru>
2025-04-05 09:49:20 +0300

    Added base sample with db work

--- onlineshop/source/server/src/app.module.ts
+++ onlineshop/source/server/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+import { DogsModule } from './dogs/dogs.module';
+import { Dog } from './dogs/entities/dog.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'sample',
+      entities: [Dog],
+      synchronize: true,
+    }),
+    CatsModule,
+    DogsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ onlineshop/source/server/src/dogs/dogs.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { CreateDogDto } from './dto/create-dog.dto';
+import { UpdateDogDto } from './dto/update-dog.dto';
+import { Dog } from './entities/dog.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class DogsService {
+  constructor(
+    @InjectRepository(Dog)
+    private dogsRepository: Repository<Dog>,
+  ) {}
+
+  create(createDogDto: CreateDogDto) {
+    return 'This action adds a new dog';
+  }
+
+  async findAll() {
+    const dogs = await this.dogsRepository.find();
+    return dogs;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} dog`;
+  }
+
+  update(id: number, updateDogDto: UpdateDogDto) {
+    return `This action updates a #${id} dog`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} dog`;
+  }
+}
